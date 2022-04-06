8c63911bde7ff12b160876fa2f1b3e917eb49461
Peter Park <peterparkk25@gmail.com>
2022-04-06 03:35:29 -0400

    Temporary API Call made to parse JSON object

--- Tabs/Rent.js
+++ Tabs/Rent.js
@@ -1,10 +1,38 @@
-import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, Button, StyleSheet, Text, View } from "react-native";
 
 export default function RentScreen({ navigation }) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    fetch(
+      "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/honda?format=json"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Cars listed here</Text>
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontSize: 18, color: "green", textAlign: "center" }}>
+            {data.Results[0].Model_Name}
+          </Text>
+        </View>
+      )}
     </View>
   );
 }

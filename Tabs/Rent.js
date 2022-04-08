010e8b6f918bcf235f6335fce1b5423f67549100
Peter Park <peterparkk25@gmail.com>
2022-04-08 06:27:57 -0400

    Make/Models dropdown functional only after refresh, working on mock login, splash, user screens in the meantime

--- Tabs/Rent.js
+++ Tabs/Rent.js
@@ -12,13 +12,15 @@ import DropDownPicker from "react-native-dropdown-picker";
 
 export default function RentScreen({ navigation }) {
   const [isLoading, setLoading] = useState(true);
-  const [open, setOpen] = useState(false);
+  const [openMakes, setOpenMakes] = useState(false);
+  const [openModels, setOpenModels] = useState(false);
   const [items, setItems] = useState([]);
   const [allMakeData, setAllMakeData] = useState([]);
   const [makeData, setMakeData] = useState([]);
   const [make, setMake] = useState(null);
   const [allModelData, setAllModelData] = useState([]);
   const [modelData, setModelData] = useState([]);
+  const [model, setModel] = useState([]);
 
   useEffect(() => {
     const allMakesURL =
@@ -36,7 +38,21 @@ export default function RentScreen({ navigation }) {
       .finally(() => setLoading(false));
   }, []);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const allModelsURL = `https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${make}?format=json`;
+  //   fetch(allModelsURL)
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setAllModelData(json);
+  //       const modelDataArray = allModelData.Results.map((model) => {
+  //         return { label: model.Model_Name, value: model.Model_Name };
+  //       });
+  //       setModelData(modelDataArray);
+  //     })
+  //     .catch((error) => console.error(error));
+  // }, [make]);
+
+  const getAllModels = () => {
     const allModelsURL = `https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${make}?format=json`;
     fetch(allModelsURL)
       .then((response) => response.json())
@@ -46,30 +62,48 @@ export default function RentScreen({ navigation }) {
           return { label: model.Model_Name, value: model.Model_Name };
         });
         setModelData(modelDataArray);
-        console.log(modelData);
       })
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, [make]);
-
-  useEffect(() => {
-    console.log(make);
-  }, [make]);
+      .catch((error) => console.error(error));
+  };
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
       {isLoading ? (
         <Text>Loading...</Text>
       ) : (
-        <DropDownPicker
-          open={open}
-          value={make}
-          items={makeData}
-          setOpen={setOpen}
-          setValue={setMake}
-          setItems={setMakeData}
-          searchable={true}
-        />
+        <View>
+          <Text>Please Choose Your Make</Text>
+          <DropDownPicker
+            open={openMakes}
+            value={make}
+            items={makeData}
+            setOpen={setOpenMakes}
+            setValue={setMake}
+            setItems={setMakeData}
+            searchable={true}
+            onChangeValue={() => {
+              getAllModels();
+            }}
+          />
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text>Please Choose Your Model</Text>
+          <DropDownPicker
+            open={openModels}
+            value={model}
+            items={modelData}
+            setOpen={setOpenModels}
+            setValue={setModel}
+            setItems={setModelData}
+            searchable={true}
+          />
+        </View>
       )}
     </View>
   );
